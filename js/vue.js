2ee49f49a5d070b254a130511c9cf36818cea3fb
ChrisjanDC <cedelacruz@student.apc.edu.ph>
2025-09-04 08:45:54 +0800

    places css and js in their own folder/file

---
+++ js/vue.js
@@ -0,0 +1,12 @@
+<script>
+  const app = Vue.createApp({
+   data() {
+    return {
+     message: "Hello Webprog World! With separate css and js files"
+    }
+   }
+  })
+  
+  app.mount('#app')
+
+</script>
