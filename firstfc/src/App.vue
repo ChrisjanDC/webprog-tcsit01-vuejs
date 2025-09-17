fee70bb058e26753acc3859a0da91eb2c3739b4b
ChrisjanDC <cedelacruz@student.apc.edu.ph>
2025-09-17 20:06:54 +0800

    fixed

--- firstfc/src/App.vue
+++ firstfc/src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Hello WEBPROG World! I'm Chrisjan</h1>
     <h2>Food</h2>
-    <food-item />
-    <food-item2 />
+    <food-item></food-item>
+    <food-item2></food-item2>
   </div>
 </template>
