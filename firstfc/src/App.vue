e09fe348b4343c03aa5c698ab8bb92380223b4d0
ChrisjanDC <cedelacruz@student.apc.edu.ph>
2025-09-17 19:49:36 +0800

    fix

--- firstfc/src/App.vue
+++ firstfc/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <h1>Hello WEBPROG AFD241 World!</h1>
+  <h2>Food</h2>
   <FoodItem />
+  <FoodItem2 /> 
 </template>
 
 <script>
 import FoodItem from './components/FoodItem.vue'
+import FoodItem2 from './components/FoodItem2.vue'
 
 export default {
   name: "App",
   components: {
-    FoodItem
+    FoodItem,
+    FoodItem2
   }
 }
 </script>
